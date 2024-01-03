20f117e596894864782ead2d6d9b71c6145e52b2
AlolaSunday12 <alolasj@gmail.com>
2024-01-03 21:50:05 +0000

    update

--- 0x01-ES6_promise/0-main.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable jest/require-hook */
-import getResponseFromAPI from "./0-promise.js";
-/* eslint-enable jest/require-hook */
-
-const response = getResponseFromAPI();
-console.log(response instanceof Promise);
--- 0x01-ES6_promise/2-main.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable jest/require-hook */
-import handleResponseFromAPI from "./2-then";
-/* eslint-enable jest/require-hook */
-
-const promise = Promise.resolve();
-handleResponseFromAPI(promise);
